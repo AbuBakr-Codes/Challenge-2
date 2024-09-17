ed58b735d06b8a4fe9330a7b082315a24691d867
AbuBakr-Codes <140212790+AbuBakr-Codes@users.noreply.github.com>
2024-09-17 12:15:47 +0100

    COMPLETED TASK
    Updated Player Controller

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timer = 1.0f; // first timer value
+    private float waitTime = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
+        //if update is called at a frame say 0.02 secs from one frame to another
+        //Delay timer by adding up to make up a resetted timer until timer goes back to 1.0f 
+        if (timer < waitTime)
+        {
+            timer += Time.deltaTime;
+        }
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        //else if is used as it requires a condition and should only happen when if statement doesn't happen
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //Resets Timer after instantiating
+            timer = 0;
         }
     }
 }
